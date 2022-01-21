dd1260b201124a9fa827b9183afd9398365e4e3f
Miwka <97275239+albertu4@users.noreply.github.com>
2022-01-21 22:12:13 +0900

    Initial Commit

---
+++ AboutMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AboutMe
+//
+//  Created by Михаил Иванов on 21.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
